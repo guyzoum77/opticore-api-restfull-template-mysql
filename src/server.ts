443a1abd236dc221ec41f78515c3bf2b3d768f64
guyzoum77 <guyzoum7700@mail.com>
2024-08-30 23:33:25 +0000

    fixing bug from kernel

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,4 @@ import "reflect-metadata";
 import {Kernel} from "../app/core/kernel";
 import {KernelModules} from "opticore-core-module";
 
-(async(): Promise<void> => {
-    const kernelLoaded = Kernel();
-    console.log("kernelLoaded is :", kernelLoaded);
-    await KernelModules(Kernel());
-})();
+(async(): Promise<void> => { await KernelModules(Kernel()); })();
