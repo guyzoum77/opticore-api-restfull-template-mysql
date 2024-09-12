0bbf4e8bf73fa7d113d12f7ba620e03fa055be70
guyzoum77 <guyzoum7700@mail.com>
2024-09-12 16:27:16 +0000

    adding optional connection argument

--- config/database.config.ts
+++ config/database.config.ts
@@ -1,7 +1,7 @@
-import { mySqlCheckerDatabase } from "opticore-core-module";
+import { mySqlCheckerDatabase, optionalArgumentConnection } from "opticore-core-module";
 
 /**
  * mySqlCheckerDatabase is function with an optional params.
- * So if you desire use an optional params define it.
+ * So if you desire use an optional params define it in .env file.
  */
-export const dbConnection = (): void => { mySqlCheckerDatabase(""); }
+export const dbConnection = (): void => { mySqlCheckerDatabase(optionalArgumentConnection); }
