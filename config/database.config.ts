4acf0960649822ec98f975f622981835c195847d
guyzoum77 <guyzoum7700@mail.com>
2024-10-05 20:47:04 +0000

    changing version in packae.json file

--- config/database.config.ts
+++ config/database.config.ts
@@ -4,6 +4,6 @@ import { mySqlCheckerDatabase, optionalArgumentConnection } from "opticore-core-
  * mySqlCheckerDatabase is function with an optional params.
  * So if you desire use an optional params define it in .env file.
  * So in your .env file, you will have :
- * ARGUMENTS_DATABASE_CONNECTION=?sslidentity=client-identity.p12&sslpassword=mypassword&sslcert=rootca.cert
+ * ARGUMENTS_DATABASE_CONNECTION=sslidentity=client-identity.p12&sslpassword=mypassword&sslcert=rootca.cert
  */
 export const dbConnection = (): void => { mySqlCheckerDatabase(optionalArgumentConnection); }
